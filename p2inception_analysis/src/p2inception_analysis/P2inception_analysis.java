f2710206b22903417c26af22979c5c3af92a2105
Cioccoffee <gallelucie@gmail.com>
2017-05-05 11:20:16 +0200

    Add files via upload

---
+++ p2inception_analysis/src/p2inception_analysis/P2inception_analysis.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p2inception_analysis;
+
+/**
+ *
+ * @author lgalle
+ */
+public class P2inception_analysis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ p2inception_analysis/src/p2inception_analysis/Query_DB.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p2inception_analysis;
+import java.sql.*;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+public class Query_DB{
+    private Connection conn;
+    private Statement stmt;
+    private ResultSet res;
+            
+    /**
+     * Methode pour se connecter Ã  la base ; prend en paramÃ¨tre le login et le mot de passe
+     */
+    public Query_DB(String nomLogin, String mdp) {
+                    try { 
+                        
+                                //Enregistrement de la classe du driver par le driverManager
+                                Class.forName("com.mysql.jdbc.Driver");
+				System.out.println("Driver trouvÃ©...");
+				 //CrÃ©ation d'une connexion sur la base de donnÃ©e
+				conn = DriverManager.getConnection("jdbc:mysql://https://www.freemysqlhosting.net:3306/sql11172818", nomLogin, mdp);
+                                System.out.println("Connexion Ã©tablie...");
+		    }
+                    catch(Exception e){
+                                    System.out.println(e.getMessage());
+                                    System.exit(0);
+                            }
+                
+                     
+                    
+            }
+
+            public static void main(String [] args) {
+		//appeler le constructeur avec vos propres login mot de passe MySQL
+                    Query_DB maBd = new Query_DB("sql11172818","c3eqIEEExh");
+		    //appel d'une mÃ©thode
+                    
+                     
+                    
+            }
+             
+             
+}
